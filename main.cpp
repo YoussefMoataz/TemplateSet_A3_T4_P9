af1dd4d6594b5cdd8a15dc8c7a3a4082751e5b78
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-12-18 15:18:43 +0200

    Added addItem function. Added getSize function.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,48 @@
 
 using namespace std;
 
-template <typename YM>
+template<typename YM>
 class YSet {
 
 private:
     vector<YM> Data;
 
 public:
+    YSet() = default;
 
+    void addItem(YM item) {
+
+        bool inSet = false;
+
+        for (int i = 0; i < Data.size(); ++i) {
+            if (Data[i] == item) {
+                inSet = true;
+            }
+        }
+
+        if (!inSet) {
+            Data.push_back(item);
+        }
+
+    }
+
+    int getSize() {
+
+        return Data.size();
+
+    }
 
 };
 
 int main() {
 
+    YSet<string> ySet;
+
+    ySet.addItem("joe");
+    cout << ySet.getSize() << endl;
+
+    ySet.addItem("hassan");
+    cout << ySet.getSize() << endl;
 
     return 0;
 }
